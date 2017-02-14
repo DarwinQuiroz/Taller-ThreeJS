4c67317209aa811722ed35218631ee34aeeda493
Darwin Quiroz <darwin_quiroz04@hotmail.com>
2017-02-14 12:42:32 -0500

    Elementos basicos

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 (function(){
+	// Definir escena
+	let scene = new THREE.Scene();
 	
+	// Definir cámara
+	const aspectRatio = window.innerWidth / window.innerHeight;
+	let camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100);
+
+	// Definir el render
+	let renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	renderer.render(scene, camera);
+
 })();
