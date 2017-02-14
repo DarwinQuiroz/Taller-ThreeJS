b4a7d3f010f8986fa3e9a30b1903b19c7ebeed8f
Darwin Quiroz <darwin_quiroz04@hotmail.com>
2017-02-14 13:47:56 -0500

    Agregar sombras

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 (function(){
 	// Definir escena
 	let scene = new THREE.Scene();
+	let mesh = null;
 	
 	// Definir cámara
 	const aspectRatio = window.innerWidth / window.innerHeight;
@@ -11,27 +12,28 @@
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	camera.position.z = 60;
+	renderer.shadowMap.enable = true; //Activar calculo de mapa 
+	renderer.shadowMap.soft = true; //Suavizar sombra
+	renderer.shadowMap.type = THREE.PCFShadowMap; //cambiar tipo de sombra
+
+	camera.position.z = 80;
 	camera.position.y = 2;
 
 	let planeGeometry = new THREE.PlaneGeometry(200,900);
 	planeGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(- Math.PI/2));
 	// let geometry = new THREE.BoxGeometry(10,10,10);
 	let grounMaterial = new THREE.MeshPhongMaterial({
-		color: 0xffffff
+		color: 0x222222
 	});
 
 	let plane = new THREE.Mesh(planeGeometry, grounMaterial);
+	plane.receiveShadow = true;
 
-
-	let mesh;
 	let loader = new THREE.TextureLoader();
 
 	loader.load('texture.jpg', function(texture){
 		let geometry = new THREE.SphereGeometry(20,100,100);
-		let material = new THREE.MeshBasicMaterial({
-			map: texture
-		});
+		let material = new THREE.MeshBasicMaterial({ map: texture });
 		mesh = new THREE.Mesh(geometry, material);
 		mesh.position.y = 25;
 		mesh.castShadow = true;
@@ -39,21 +41,24 @@
 		// add to the scene
 		scene.add(mesh);
 	})
-
-
 	
-	// let mesh = new THREE.Mesh(geometry, grounMaterial);
-
 	let pointLight = new THREE.PointLight(0xdfebff);
-	pointLight.position.y = 30;
+	pointLight.position.y = 60;
 	pointLight.position.z = 20;
+	pointLight.castShadow = true;
 
-	// scene.background = new THREE.Color(0xeeeeee);
-	// scene.add(mesh);
-	// scene.add(new THREE.AmbientLight(0x404040));
+	let helper = new THREE.CameraHelper(pointLight.shadow.camera);
+
+	scene.background = new THREE.Color(0xeeeeee);
+	scene.add(helper);
+
 	scene.add(plane);
 	scene.add(pointLight);
 
+	scene.add(new THREE.AmbientLight(0x404040));
+
+
+	let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 	function loop()
 	{
